7c66b4eaafb5877dfbb4c9039fdfb8894301a31c
abdu61 <112977047+abdu61@users.noreply.github.com>
2024-05-07 22:07:08 +0300

    Refactor staff_nav

--- lib/navigation/staff_nav.dart
+++ lib/navigation/staff_nav.dart
@@ -3,16 +3,13 @@ import 'package:dental_clinic/screens/staff/staff_chat.dart';
 import 'package:dental_clinic/screens/staff/staff_home.dart';
 import 'package:dental_clinic/services/auth.dart';
 import 'package:dental_clinic/services/database.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class StaffNav extends StatefulWidget {
-  StaffNav({Key? key}) : super(key: key);
-
   @override
-  State<StaffNav> createState() => _StaffNavState();
+  _StaffNavState createState() => _StaffNavState();
 }
 
 class _StaffNavState extends State<StaffNav> {
@@ -20,66 +17,45 @@ class _StaffNavState extends State<StaffNav> {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
   DatabaseService? db;
   bool loading = false;
-  late Future<String> userName;
-  String? userId;
 
   @override
   void initState() {
     super.initState();
-
     final User? user = _firebaseAuth.currentUser;
     if (user != null) {
       db = DatabaseService(uid: user.uid);
     }
-
-    userName = getUserName();
-    userId = _firebaseAuth.currentUser?.uid;
-  }
-
-  Future<String> getUserName() async {
-    final User? user = _firebaseAuth.currentUser;
-    final DocumentSnapshot docSnapshot = await db!.getUserData();
-    return (docSnapshot.data() as Map<String, dynamic>)['name'] ?? 'User';
   }
 
   @override
   Widget build(BuildContext context) {
-    bool loading = false;
     final controller = Get.put(StaffNavigationController());
+
     return Scaffold(
       appBar: AppBar(
-        title: const DefaultTextStyle(
-          style: TextStyle(
-            color: Colors.black,
-            fontWeight: FontWeight.bold,
-            fontSize: 25.0,
-          ),
-          child: Text('Staff Home'),
-        ),
+        title: const Text('Staff Home',
+            style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontSize: 25.0)),
         backgroundColor: const Color.fromARGB(255, 220, 227, 255),
         elevation: 0.0,
         actions: <Widget>[
           TextButton.icon(
             onPressed: () async {
-              //sign out
               setState(() => loading = true);
               await auth.signOut();
               if (mounted) {
                 setState(() => loading = false);
               }
             },
-            icon: const Icon(
-              Icons.logout_outlined,
-              color: Color.fromARGB(255, 0, 0, 0),
-            ),
-            label: const Text(
-              'logout',
-              style: TextStyle(
-                fontSize: 16.0,
-                color: Color.fromARGB(255, 0, 0, 0),
-                fontWeight: FontWeight.bold,
-              ),
-            ),
+            icon: const Icon(Icons.logout_outlined,
+                color: Color.fromARGB(255, 0, 0, 0)),
+            label: const Text('logout',
+                style: TextStyle(
+                    fontSize: 16.0,
+                    color: Color.fromARGB(255, 0, 0, 0),
+                    fontWeight: FontWeight.bold)),
           ),
         ],
       ),
@@ -99,45 +75,27 @@ class _StaffNavState extends State<StaffNav> {
                   labelType: NavigationRailLabelType.all,
                   destinations: const [
                     NavigationRailDestination(
-                      icon: Icon(
-                        Icons.home_outlined,
-                        size: 30.0,
-                      ),
-                      label: Text(
-                        'Home',
-                        style: TextStyle(
-                          fontSize: 14.0,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                    ),
+                        icon: Icon(Icons.home_outlined, size: 30.0),
+                        label: Text('Home',
+                            style: TextStyle(
+                                fontSize: 14.0, fontWeight: FontWeight.w500))),
                     NavigationRailDestination(
-                      icon: Icon(Icons.calendar_today_outlined, size: 26.0),
-                      label: Text(
-                        'Appointment',
-                        style: TextStyle(
-                          fontSize: 14.0,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                    ),
+                        icon: Icon(Icons.calendar_today_outlined, size: 26.0),
+                        label: Text('Appointment',
+                            style: TextStyle(
+                                fontSize: 14.0, fontWeight: FontWeight.w500))),
                     NavigationRailDestination(
-                      icon: Icon(Icons.chat_outlined, size: 26.0), // Chat icon
-                      label: Text(
-                        'Chat',
-                        style: TextStyle(
-                          fontSize: 14.0,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                    ),
+                        icon: Icon(Icons.chat_outlined, size: 26.0),
+                        label: Text('Chat',
+                            style: TextStyle(
+                                fontSize: 14.0, fontWeight: FontWeight.w500))),
                   ],
                 )),
           ),
           const VerticalDivider(thickness: 1, width: 1),
           Expanded(
             child: db == null
-                ? CircularProgressIndicator() // Loading indicator
+                ? CircularProgressIndicator()
                 : Obx(() => controller.screens[controller.selectedIndex.value](
                     auth, db!)),
           ),
