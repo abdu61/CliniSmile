9ecb375e1cc6fbdeb8c521eb7006aaa6f74fd4e4
abdu61 <112977047+abdu61@users.noreply.github.com>
2024-05-07 22:02:12 +0300

    Refactor guest_nav

--- lib/navigation/guest_nav.dart
+++ lib/navigation/guest_nav.dart
@@ -5,58 +5,56 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class GuestNav extends StatelessWidget {
-  const GuestNav({super.key});
+  const GuestNav({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // Initialize the navigation controller
     final controller = Get.put(NavigationController());
+
     return Scaffold(
-      // The bottom navigation bar is an Obx widget that reacts to changes in the controller's selectedIndex
-      bottomNavigationBar: Obx(
-        () => NavigationBar(
-          height: 80,
-          elevation: 0.0,
-          // The selected index is the current value of the controller's selectedIndex
-          selectedIndex: controller.selectedIndex.value,
-          // When a navigation item is selected, update the controller's selectedIndex
-          onDestinationSelected: (index) =>
-              controller.selectedIndex.value = index,
-          // Define the navigation destinations
-          destinations: const [
-            NavigationDestination(
-              icon: Icon(Icons.home),
-              label: 'Home',
-            ),
-            NavigationDestination(
-              icon: Icon(Icons.feed),
-              label: 'Feed',
-            ),
-            NavigationDestination(
-              icon: Icon(Icons.person),
-              label: 'Profile',
-            ),
-          ],
-          backgroundColor: Colors.white,
-          indicatorColor: const Color.fromARGB(255, 152, 176, 255),
-        ),
-      ),
-      // The body is an Obx widget that reacts to changes in the controller's selectedIndex
-      // It displays the screen corresponding to the current selectedIndex
-      body: Obx(() => controller.screens[controller.selectedIndex.value]),
+      bottomNavigationBar: buildBottomNavigationBar(controller),
+      body: buildBody(controller),
     );
+  }
+
+  Widget buildBottomNavigationBar(NavigationController controller) {
+    return Obx(
+      () => NavigationBar(
+        height: 80,
+        elevation: 0.0,
+        selectedIndex: controller.selectedIndex.value,
+        onDestinationSelected: (index) =>
+            controller.selectedIndex.value = index,
+        destinations: buildNavigationDestinations(),
+        backgroundColor: Colors.white,
+        indicatorColor: const Color.fromARGB(255, 152, 176, 255),
+      ),
+    );
+  }
+
+  List<NavigationDestination> buildNavigationDestinations() {
+    return const [
+      NavigationDestination(
+        icon: Icon(Icons.home),
+        label: 'Home',
+      ),
+      NavigationDestination(
+        icon: Icon(Icons.feed),
+        label: 'Feed',
+      ),
+      NavigationDestination(
+        icon: Icon(Icons.person),
+        label: 'Profile',
+      ),
+    ];
+  }
+
+  Widget buildBody(NavigationController controller) {
+    return Obx(() => controller.screens[controller.selectedIndex.value]);
   }
 }
 
-// Define a GetxController for the navigation
 class NavigationController extends GetxController {
-  // Define a reactive integer for the selected index
   final Rx<int> selectedIndex = 0.obs;
-
-  // Define the screens for each navigation destination
-  final screens = [
-    Home(),
-    Feed(),
-    GuestProfile(),
-  ];
+  final screens = [Home(), Feed(), GuestProfile()];
 }
